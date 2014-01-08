fe78fa49f8334769780e91e01b38f44cfbb63bce
saf <safiire@gmail.com>
2014-01-07 23:26:56 -0800

    Made it concurrently calculate every pixel at once, (probably not a good idea)

--- main.rs
+++ main.rs
@@ -1,5 +1,5 @@
 extern mod extra;
-use color::{Color};
+use color::{Color, RGB};
 use netpbm::{PPM};
 use viewport::{ViewPort};
 use complex_math::{Sine};
@@ -9,13 +9,45 @@ mod viewport;
 mod complex_math;
 
 
-//  Main
-fn main() {
+enum Packet {
+  Pixel(uint, uint, RGB),
+  Done
+}
+
+
+//  PPM Writer Task
+fn ppm_write_worker(port: Port<Packet>) -> () {
   let width = 1024;
   let height = 1024;
 
   //  Create a ppm image to store the output
   let mut ppm = PPM::new(width, height);
+
+  loop {
+    match port.recv() {
+      Pixel(x, y, rgb) => {
+        //  Write the pixel into the PPM 
+        ppm.set_pixel(x, y, rgb);
+        //println!("Got thing {}, {}, {:?}", x, y, rgb);
+      },
+      Done => {
+        //  Write the image to disk
+        ppm.write_file("output.ppm");
+        return;
+      }
+    }
+  }
+}
+
+
+//  Main
+fn main() {
+  let width = 1024;
+  let height = 1024;
+
+  let (port, chan): (Port<Packet>, SharedChan<Packet>) = SharedChan::new();
+
+  do spawn || { ppm_write_worker(port) }
 
   //  Create a viewport into our complex plane
   let viewport = ViewPort::new(width, height, -3.0, 3.0, 3.0, -3.0);
@@ -24,15 +56,17 @@ fn main() {
   for y in range(0, height) {
     for x in range(0, width) {
 
-      let mut z = viewport.pixel_to_complex(x, y);
-      z = z.sine();
+      let my_chan = chan.clone();
+      do spawn || {
+        let mut z = viewport.pixel_to_complex(x, y);
 
-      let rgb = z.to_rgb();
-      ppm.set_pixel(x, y, rgb);
+        z = z.sine();
+        let rgb = z.to_rgb();
+
+        my_chan.send(Pixel(x, y, rgb));
+      }
     }
   }
-
-  //  Write the image to disk
-  ppm.write_file("output.ppm");
+  chan.send(Done);
 }
 
